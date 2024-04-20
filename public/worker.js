60406b8d8e2d1c96544e49342abcd95e29aa663c
Brian Yau <brianyauu@gmail.com>
2024-04-20 21:49:35 +0800

    update service worker

--- public/worker.js
+++ public/worker.js
@@ -1,11 +1,15 @@
-const cacheName = 'v1'
+const cacheName = 'v2'
 
 const cacheClone = async (e) => {
   const res = await fetch(e.request)
-  const resClone = res.clone()
 
-  const cache = await caches.open(cacheName)
-  await cache.put(e.request, resClone)
+  // Make a clone of the response if it's from the same origin
+  if (e.request.url.startsWith(window.location.origin)) {
+    const resClone = res.clone()
+
+    const cache = await caches.open(cacheName)
+    await cache.put(e.request, resClone)
+  }
   return res
 }
 
@@ -13,7 +17,10 @@ const fetchEvent = () => {
   self.addEventListener('fetch', (e) => {
     e.respondWith(
       cacheClone(e)
-        .catch(() => caches.match(e.request))
+        .catch((err) => {
+          console.debug(err)
+          caches.match(e.request)
+        })
         .then((res) => res),
     )
   })
